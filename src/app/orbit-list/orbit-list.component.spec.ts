658956938e171947b856285a896f555bf959d5b6
Cal Colson <cpcolson@yahoo.com>
2019-10-23 15:26:32 -0500

    Pass in Satellites to Orbit List Component

--- src/app/orbit-list/orbit-list.component.spec.ts
+++ src/app/orbit-list/orbit-list.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { OrbitListComponent } from './orbit-list.component';
+import { OrbitListComponent } from './orbit-list.component'
 
 describe('OrbitListComponent', () => {
-  let component: OrbitListComponent;
-  let fixture: ComponentFixture<OrbitListComponent>;
+  let component: OrbitListComponent
+  let fixture: ComponentFixture<OrbitListComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ OrbitListComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(OrbitListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(OrbitListComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
