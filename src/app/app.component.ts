cd57c966c3074f1ed36ed30db84e060d05bb14ab
Cal Colson <cpcolson@yahoo.com>
2019-10-23 19:04:07 -0500

    finish bonus assignments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,22 @@ export class AppComponent {
   title = 'orbit-report'
   sourceList: Satellite[]
   displayList: Satellite[]
+  typeInfos
 
   constructor() {
     this.sourceList = []
     this.displayList = this.sourceList
+
+    this.typeInfos = [
+      { typeName: 'Total', count: 0 },
+      { typeName: 'Space Debris', count: 0 },
+      { typeName: 'Communication', count: 0 },
+      { typeName: 'Probe', count: 0 },
+      { typeName: 'Positioning', count: 0 },
+      { typeName: 'Space Station', count: 0 },
+      { typeName: 'Telescope', count: 0 },
+    ]
+
     const satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json'
 
     window.fetch(satellitesUrl).then(function(response: Response) {
@@ -27,8 +39,17 @@ export class AppComponent {
           const curSatellite = new Satellite(satellite.name, satellite.type, satellite.launchDate, satellite.orbitType, satellite.operational)
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
           this.sourceList.push(curSatellite)
+          // console.log(this.sourceList)
+
+          for (const typeInfo of this.typeInfos) {
+            if (typeInfo.typeName === curSatellite.type) {
+              typeInfo.count++
+              // Increment total
+              this.typeInfos[0].count++
+            }
+          }
         }
-        // console.log(this.sourceList)
+        // console.log(this.typeInfos)
       }.bind(this))
     }.bind(this))
   }
@@ -41,7 +62,36 @@ export class AppComponent {
       if (name.indexOf(searchTerm) >= 0) {
         matchingSatellites.push(satellite)
       }
+      const orbitType = satellite.orbitType.toLowerCase()
+      if (orbitType.indexOf(searchTerm) >= 0 && !matchingSatellites.includes(satellite)) {
+        matchingSatellites.push(satellite)
+      }
+      const type = satellite.type.toLowerCase()
+      if (type.indexOf(searchTerm) >= 0 && !matchingSatellites.includes(satellite)) {
+        matchingSatellites.push(satellite)
+      }
     }
     this.displayList = matchingSatellites
+
+    // Type count display code
+    this.typeInfos = [
+      { typeName: 'Total', count: 0 },
+      { typeName: 'Space Debris', count: 0 },
+      { typeName: 'Communication', count: 0 },
+      { typeName: 'Probe', count: 0 },
+      { typeName: 'Positioning', count: 0 },
+      { typeName: 'Space Station', count: 0 },
+      { typeName: 'Telescope', count: 0 },
+    ]
+
+    for (const satellite of this.displayList) {
+      for (const typeInfo of this.typeInfos) {
+        if (typeInfo.typeName === satellite.type) {
+          typeInfo.count++
+          // Increment total
+          this.typeInfos[0].count++
+        }
+      }
+    }
   }
 }
